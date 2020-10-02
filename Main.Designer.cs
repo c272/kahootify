94c3e75fb0e23005e58837fdf6732fe50edc8a8e
C272 <realrussianmanconfirmed@gmail.com>
2020-10-02 16:02:55 +0100

    Update with click-and-drag region select.

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -30,29 +30,28 @@
         {
             this.startAnalysisBtn = new System.Windows.Forms.Button();
             this.stopAnalysisBtn = new System.Windows.Forms.Button();
-            this.tlcTxt = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.brcTxt = new System.Windows.Forms.TextBox();
             this.currentPosLbl = new System.Windows.Forms.Label();
             this.screenRegionPB = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.statusLbl = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.guessTxt = new System.Windows.Forms.Label();
-            this.searchBtn = new System.Windows.Forms.Button();
             this.browser = new System.Windows.Forms.WebBrowser();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.guessTxt = new System.Windows.Forms.Label();
+            this.regionSelectBtn = new System.Windows.Forms.Button();
+            this.regionSelectPB = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.screenRegionPB)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.regionSelectPB)).BeginInit();
             this.SuspendLayout();
             // 
             // startAnalysisBtn
             // 
-            this.startAnalysisBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.startAnalysisBtn.Location = new System.Drawing.Point(874, 15);
+            this.startAnalysisBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.startAnalysisBtn.Location = new System.Drawing.Point(15, 530);
             this.startAnalysisBtn.Name = "startAnalysisBtn";
-            this.startAnalysisBtn.Size = new System.Drawing.Size(111, 51);
+            this.startAnalysisBtn.Size = new System.Drawing.Size(110, 43);
             this.startAnalysisBtn.TabIndex = 0;
             this.startAnalysisBtn.Text = "Start Analysis";
             this.startAnalysisBtn.UseVisualStyleBackColor = true;
@@ -60,63 +59,33 @@
             // 
             // stopAnalysisBtn
             // 
-            this.stopAnalysisBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.stopAnalysisBtn.Location = new System.Drawing.Point(991, 15);
+            this.stopAnalysisBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.stopAnalysisBtn.Location = new System.Drawing.Point(131, 530);
             this.stopAnalysisBtn.Name = "stopAnalysisBtn";
-            this.stopAnalysisBtn.Size = new System.Drawing.Size(111, 51);
+            this.stopAnalysisBtn.Size = new System.Drawing.Size(120, 43);
             this.stopAnalysisBtn.TabIndex = 1;
             this.stopAnalysisBtn.Text = "Stop Analysis";
             this.stopAnalysisBtn.UseVisualStyleBackColor = true;
             this.stopAnalysisBtn.Click += new System.EventHandler(this.stopAnalysisBtn_Click);
             // 
-            // tlcTxt
-            // 
-            this.tlcTxt.Location = new System.Drawing.Point(155, 15);
-            this.tlcTxt.Name = "tlcTxt";
-            this.tlcTxt.Size = new System.Drawing.Size(100, 22);
-            this.tlcTxt.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Top Left Corner:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 44);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(140, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Bottom Right Corner:";
-            // 
-            // brcTxt
-            // 
-            this.brcTxt.Location = new System.Drawing.Point(155, 43);
-            this.brcTxt.Name = "brcTxt";
-            this.brcTxt.Size = new System.Drawing.Size(100, 22);
-            this.brcTxt.TabIndex = 4;
-            // 
             // currentPosLbl
             // 
+            this.currentPosLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.currentPosLbl.AutoSize = true;
             this.currentPosLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.currentPosLbl.Location = new System.Drawing.Point(267, 17);
+            this.currentPosLbl.Location = new System.Drawing.Point(13, 601);
             this.currentPosLbl.Name = "currentPosLbl";
             this.currentPosLbl.Size = new System.Drawing.Size(158, 17);
             this.currentPosLbl.TabIndex = 6;
             this.currentPosLbl.Text = "Current mouse position:";
+            this.currentPosLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // screenRegionPB
             // 
             this.screenRegionPB.Dock = System.Windows.Forms.DockStyle.Fill;
             this.screenRegionPB.Location = new System.Drawing.Point(3, 18);
             this.screenRegionPB.Name = "screenRegionPB";
-            this.screenRegionPB.Size = new System.Drawing.Size(357, 571);
+            this.screenRegionPB.Size = new System.Drawing.Size(357, 491);
             this.screenRegionPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.screenRegionPB.TabIndex = 7;
             this.screenRegionPB.TabStop = false;
@@ -126,22 +95,24 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox1.Controls.Add(this.screenRegionPB);
-            this.groupBox1.Location = new System.Drawing.Point(12, 78);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(363, 592);
+            this.groupBox1.Size = new System.Drawing.Size(363, 512);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Region Preview";
             // 
             // statusLbl
             // 
+            this.statusLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.statusLbl.AutoSize = true;
             this.statusLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusLbl.Location = new System.Drawing.Point(266, 41);
+            this.statusLbl.Location = new System.Drawing.Point(12, 581);
             this.statusLbl.Name = "statusLbl";
             this.statusLbl.Size = new System.Drawing.Size(183, 20);
             this.statusLbl.TabIndex = 9;
             this.statusLbl.Text = "Current status: OFF.";
+            this.statusLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // groupBox2
             // 
@@ -151,23 +122,23 @@
             this.groupBox2.Controls.Add(this.browser);
             this.groupBox2.Controls.Add(this.searchBtn);
             this.groupBox2.Controls.Add(this.guessTxt);
-            this.groupBox2.Location = new System.Drawing.Point(381, 78);
+            this.groupBox2.Location = new System.Drawing.Point(381, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(722, 592);
+            this.groupBox2.Size = new System.Drawing.Size(722, 610);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Results";
             // 
-            // guessTxt
+            // browser
             // 
-            this.guessTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.guessTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guessTxt.Location = new System.Drawing.Point(10, 27);
-            this.guessTxt.Name = "guessTxt";
-            this.guessTxt.Size = new System.Drawing.Size(580, 34);
-            this.guessTxt.TabIndex = 0;
-            this.guessTxt.Text = "Current best guess is \"\".";
+            this.browser.Location = new System.Drawing.Point(6, 63);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(710, 541);
+            this.browser.TabIndex = 2;
             // 
             // searchBtn
             // 
@@ -181,37 +152,60 @@
             this.searchBtn.UseVisualStyleBackColor = true;
             this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
-            // browser
+            // guessTxt
             // 
-            this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.guessTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.browser.Location = new System.Drawing.Point(6, 64);
-            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.browser.Name = "browser";
-            this.browser.Size = new System.Drawing.Size(710, 522);
-            this.browser.TabIndex = 2;
+            this.guessTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessTxt.Location = new System.Drawing.Point(10, 27);
+            this.guessTxt.Name = "guessTxt";
+            this.guessTxt.Size = new System.Drawing.Size(580, 34);
+            this.guessTxt.TabIndex = 0;
+            this.guessTxt.Text = "Current best guess is \"\".";
+            // 
+            // regionSelectBtn
+            // 
+            this.regionSelectBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.regionSelectBtn.Location = new System.Drawing.Point(257, 530);
+            this.regionSelectBtn.Name = "regionSelectBtn";
+            this.regionSelectBtn.Size = new System.Drawing.Size(118, 43);
+            this.regionSelectBtn.TabIndex = 11;
+            this.regionSelectBtn.Text = "Select Region";
+            this.regionSelectBtn.UseVisualStyleBackColor = true;
+            this.regionSelectBtn.Click += new System.EventHandler(this.regionSelectBtn_Click);
+            // 
+            // regionSelectPB
+            // 
+            this.regionSelectPB.Location = new System.Drawing.Point(0, 0);
+            this.regionSelectPB.Margin = new System.Windows.Forms.Padding(0);
+            this.regionSelectPB.Name = "regionSelectPB";
+            this.regionSelectPB.Size = new System.Drawing.Size(50, 50);
+            this.regionSelectPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.regionSelectPB.TabIndex = 12;
+            this.regionSelectPB.TabStop = false;
+            this.regionSelectPB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.regionSelect_MouseDown);
+            this.regionSelectPB.MouseMove += new System.Windows.Forms.MouseEventHandler(this.regionSelect_MouseMove);
+            this.regionSelectPB.MouseUp += new System.Windows.Forms.MouseEventHandler(this.regionSelect_MouseUp);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1115, 682);
+            this.ClientSize = new System.Drawing.Size(1115, 635);
+            this.Controls.Add(this.regionSelectPB);
+            this.Controls.Add(this.regionSelectBtn);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.statusLbl);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.currentPosLbl);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.brcTxt);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tlcTxt);
             this.Controls.Add(this.stopAnalysisBtn);
             this.Controls.Add(this.startAnalysisBtn);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Text = "Kahootify - Search and Destroy";
             ((System.ComponentModel.ISupportInitialize)(this.screenRegionPB)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.regionSelectPB)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,10 +215,6 @@
 
         private System.Windows.Forms.Button startAnalysisBtn;
         private System.Windows.Forms.Button stopAnalysisBtn;
-        private System.Windows.Forms.TextBox tlcTxt;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox brcTxt;
         private System.Windows.Forms.Label currentPosLbl;
         private System.Windows.Forms.PictureBox screenRegionPB;
         private System.Windows.Forms.GroupBox groupBox1;
@@ -233,6 +223,8 @@
         private System.Windows.Forms.Button searchBtn;
         private System.Windows.Forms.Label guessTxt;
         private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.Button regionSelectBtn;
+        private System.Windows.Forms.PictureBox regionSelectPB;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -14,18 +16,34 @@ namespace kahootify
 {
     public partial class Main : Form
     {
+        //Timers for repeated events.
         Timer analysisTimer = new Timer();
         Timer mousePosTimer = new Timer() { Interval = 100 };
-        Regex validCoord = new Regex("^[0-9]+( )?,( )?[0-9]+$");
+        Rectangle regionRect = default;
         public string guess = "";
+
+        //Mouse position variables for region selection.
+        int selectX, selectY, selectWidth, selectHeight;
+        public Pen selectPen;
+
+        //This variable control when you start the right click
+        bool startedClicking = false;
 
         public Main()
         {
             InitializeComponent();
+
+            //Start timers for repeated events.
             analysisTimer.Interval = 1000;
             analysisTimer.Tick += analyzeScreen;
             mousePosTimer.Tick += updateMousePos;
             mousePosTimer.Start();
+
+            //Hide the region select picture box.
+            regionSelectPB.Hide();
+
+            //Disable the "start analysis" button (no region).
+            startAnalysisBtn.Enabled = false;
         }
 
         /// <summary>
@@ -41,28 +59,13 @@ namespace kahootify
         /// </summary>
         private void analyzeScreen(object sender, EventArgs e)
         {
-            //Are the coordinates valid?
-            if (!validCoord.IsMatch(tlcTxt.Text) || !validCoord.IsMatch(brcTxt.Text))
-            {
-                MessageBox.Show("Invalid screen coordinates given. Must be in form 'num, num'.");
-                analysisTimer.Stop();
-                stopAnalysisBtn_Click(this, null);
-                return;
-            }
+            //Has a region been selected yet?
+            if (regionRect == default(Rectangle)) { return; }
 
-            //Get rectangle out.
-            string[] topLeftNums = tlcTxt.Text.Replace(" ", "").Split(',');
-            string[] bottomRightNums = brcTxt.Text.Replace(" ", "").Split(',');
-            int[] nums = { 
-                               int.Parse(topLeftNums[0]), int.Parse(topLeftNums[1]),
-                               int.Parse(bottomRightNums[0]), int.Parse(bottomRightNums[1])
-                             };
-
-            //Get the correct portion of the screen.
-            var rect = new Rectangle(nums[0], nums[1], nums[2] - nums[0], nums[3] - nums[1]);
-            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+            //Get screen area out.
+            Bitmap bmp = new Bitmap(regionRect.Width, regionRect.Height, PixelFormat.Format32bppArgb);
             Graphics g = Graphics.FromImage(bmp);
-            g.CopyFromScreen(rect.X, rect.Y, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            g.CopyFromScreen(regionRect.X, regionRect.Y, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
             screenRegionPB.Image = bmp;
 
             //OCR.
@@ -101,6 +104,121 @@ namespace kahootify
         private void searchBtn_Click(object sender, EventArgs e)
         {
             browser.Navigate("https://www.google.com/search?q=" + guess);
+        }
+
+        /// <summary>
+        /// Allows the user to select a region of the screen to capture.
+        /// </summary>
+        private void regionSelectBtn_Click(object sender, EventArgs e)
+        {
+            //Hide form, make a bitmap of the screen.
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Hide();
+            Bitmap screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            //Create graphics with the bitmap, copy.
+            Graphics graphics = Graphics.FromImage((Image)screen);
+            graphics.CopyFromScreen(0, 0, 0, 0, screen.Size);
+
+            //Create a temporary memory stream and save the graphics into it, set 
+            using (MemoryStream s = new MemoryStream())
+            {
+                screen.Save(s, ImageFormat.Bmp);
+                regionSelectPB.Size = new Size(this.Width, this.Height);
+                regionSelectPB.Image = Image.FromStream(s);
+            }
+
+            //Show and set cursor.
+            regionSelectPB.Show();
+            this.Show();
+            Cursor = Cursors.Cross;
+        }
+
+        /// <summary>
+        /// Triggered when the user moves the mouse over the region select box.
+        /// </summary>
+        private void regionSelect_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Image exists?
+            if (regionSelectPB.Image == null)
+            {
+                return;
+            }
+
+            //Has the user started dragging?
+            if (startedClicking)
+            {
+                //Refresh picture box, set coordinate square corner.
+                regionSelectPB.Refresh();
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                regionSelectPB.CreateGraphics().DrawRectangle(selectPen, selectX, selectY, selectWidth, selectHeight);
+            }
+        }
+        
+        /// <summary>
+        /// Triggered when the user clicks down on the region select box.
+        /// </summary>
+        private void regionSelect_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Has the user already started clicking?
+            if (startedClicking) { return; }
+
+            //Is LMB down?
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                startedClicking = true;
+
+                //Get start coords for rectangle.
+                selectX = e.X;
+                selectY = e.Y;
+                selectPen = new Pen(Color.Red, 1);
+                selectPen.DashStyle = DashStyle.DashDotDot;
+            }
+
+            //Refresh picture box, start clicking.
+            regionSelectPB.Refresh();
+            return;
+        }
+
+        /// <summary>
+        /// Triggered when the user releases a mouse button on the region select box.
+        /// </summary>
+        private void regionSelect_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Make sure the user's started to click already.
+            if (!startedClicking) { return; }
+
+            //Make sure an image exists.
+            if (regionSelectPB.Image == null) { return; }
+
+            //Set the width/height, etc.
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                regionSelectPB.Refresh();
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                regionSelectPB.CreateGraphics().DrawRectangle(selectPen, selectX,
+                         selectY, selectWidth, selectHeight);
+
+            }
+            startedClicking = false;
+
+            //Reset window mode, etc.
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            regionSelectPB.Hide();
+            Cursor = Cursors.Default;
+
+            //Set the region rectangle.
+            if (selectWidth > 0)
+            {
+                regionRect = new Rectangle(selectX, selectY, selectWidth, selectHeight);
+
+                //Re-enable analysis button.
+                startAnalysisBtn.Enabled = true;
+            }
         }
     }
 }
